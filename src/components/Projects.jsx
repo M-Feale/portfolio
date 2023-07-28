18be39712404a76792c6d8811208e07632378af8
M-Feale <marjolainefeale@gmail.com>
2023-07-27 22:21:27 -0400

    finalised-design

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,23 +12,29 @@ const Projects = () => {
 						Learning something new at every opportunity.
 					</Tagline>
 					<Tagline>Pushing myself outside of my comfort zone.</Tagline>
-					<Tagline>Thriving.</Tagline>
+					<Tagline><GreenSpan>Thriving.</GreenSpan></Tagline>
 				</TitleTextDiv>
 			</MainContainer>
 			<SecondMainContainer>
-				<Call>Legend of Burger - A NyanCat to the Past</Call>
+				<Headline>Legend of Burger - A NyanCat to the Past</Headline>
 				<Tiles />
 			</SecondMainContainer>
 		</>
 	);
 };
 
+const GreenSpan = styled.span`
+    background-color: var(--tertiary-color);
+    color: var(--primary-color);
+    padding: 5px 10px;
+    font-weight: bold;
+`;
+
 const MainContainer = styled.div`
 	width: 100vw;
 	max-width: 100%;
 	margin: 0 auto;
-	height: 75vh; // the logic here is that the header is 15vh so to make the page 100vh,
-	//I need to substract the two.
+	height: 75vh; // the logic here is that the header is 15vh so for people to KNOW that they have to scroll, I'm making it just a bit too small so next component is showing.
 
 	background-color: var(--secondary-color);
 	display: flex;
