e082a0ae9677e5dbfaa5fc39449a4fbead0b71c2
M-Feale <marjolainefeale@gmail.com>
2023-07-28 22:50:27 -0400

    modified-feature-structure

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,7 +19,6 @@ const Contact = () => {
 		) {
 			setIsDisplayed(true);
 		}
-		s;
 	};
 
 	const [isDisplayed, setIsDisplayed] = useState(false);
@@ -36,97 +35,106 @@ const Contact = () => {
 				</TitleTextDiv>
 			</MainContainer>
 			<SecondMainContainer>
-				<ImageTitleWrapper>
-					<Headline>Stay in touch</Headline>
-				</ImageTitleWrapper>
-
-				<FormDiv>
-					<TileTitle>Connect with Me !</TileTitle>
-					<form>
-						<InputDiv>
-							<Label htmlFor="name">Name</Label>
-							<InputField
-								required
-								id="name"
-								type="text"
-								value={formData.name}
-								onChange={(e) => {
-									console.log(e.target.value);
-									setFormData({
-										...formData,
-										name: e.target.value,
-									});
-								}}
-							/>
-							<Label htmlFor="email">Email address</Label>
-							<InputField
-								required
-								id="email"
-								type="email"
-								value={formData.email}
-								onChange={(e) => {
-									setFormData({
-										...formData,
-										email: e.target.value,
-									});
-								}}
-							/>
-							<Label htmlFor="interest">You are a...</Label>
-							<Select
-								required
-								id="interest"
-								value={formData.interest}
-								onChange={(e) => {
-									setFormData({
-										...formData,
-										interest: e.target.value,
-									});
-								}}
-							>
-								<Option value="fellow web developer">
-									Fellow Web developer
-								</Option>
-								<Option value="recruiter">Recruiter</Option>
-								<Option value="curious person">Curious</Option>
-							</Select>
-							<Label htmlFor="message">Message</Label>
-							<Textarea
-								required
-								id="message"
-								type="textarea"
-								value={formData.message}
-								onChange={(e) => {
-									setFormData({
-										...formData,
-										message: e.target.value,
-									});
-								}}
-							></Textarea>
-						</InputDiv>
-						<ButtonDiv>
-							<SubmitButton onClick={handleSubmit} type="button">
-								Submit
-							</SubmitButton>
-						</ButtonDiv>
-					</form>
-				</FormDiv>
-				{isDisplayed && (
-					<MessageDiv>
-						<SubmitText>
-							Hi {formData.name} ! Nice to meet you !
-						</SubmitText>
-						<SubmitText>
-							I'm always happy to meet a {formData.interest}.
-						</SubmitText>
-						<SubmitText>
-							I will contact you shortly by email at{" "}
-							{formData.email} so we can discuss your message. 
-						</SubmitText>
-            <SubmitText>
-              "{formData.message}"
-            </SubmitText>
-					</MessageDiv>
-				)}
+				<LeftDiv>
+					<ImageTitleWrapper>
+						<Headline>Stay in touch</Headline>
+					</ImageTitleWrapper>
+					{!isDisplayed && (
+						<MessageDiv>
+							<SubmitText>
+								Hi {formData.name} ! Nice to meet you !
+							</SubmitText>
+							<SubmitText>
+								I'm always happy to meet a {formData.interest}.
+							</SubmitText>
+							<SubmitText>
+								I will contact you shortly by email at{" "}
+								{formData.email} so we can discuss your message.
+							</SubmitText>
+							<SubmitText>"{formData.message}"</SubmitText>
+						</MessageDiv>
+					)}
+				</LeftDiv>
+				<RightDiv>
+					<FormDiv>
+						<TileTitle>Send me a message</TileTitle>
+						<form>
+							<InputDiv>
+								<Label htmlFor="name">Name</Label>
+								<InputField
+									required
+									id="name"
+									type="text"
+									value={formData.name}
+									onChange={(e) => {
+										console.log(e.target.value);
+										setFormData({
+											...formData,
+											name: e.target.value,
+										});
+									}}
+								/>
+								<Label htmlFor="email">Email address</Label>
+								<InputField
+									required
+									id="email"
+									type="email"
+									value={formData.email}
+									onChange={(e) => {
+										setFormData({
+											...formData,
+											email: e.target.value,
+										});
+									}}
+								/>
+								<Label htmlFor="interest">You are a...</Label>
+								<Select
+									required
+									id="interest"
+									value={formData.interest}
+									onChange={(e) => {
+										setFormData({
+											...formData,
+											interest: e.target.value,
+										});
+									}}
+								>
+									<Option value="fellow web developer">
+										Fellow Web developer
+									</Option>
+									<Option value="recruiter">Recruiter</Option>
+									<Option value="curious person">
+										Curious
+									</Option>
+								</Select>
+								<Label htmlFor="message">Message</Label>
+								<Textarea
+									required
+									id="message"
+									type="textarea"
+									value={formData.message}
+									onChange={(e) => {
+										setFormData({
+											...formData,
+											message: e.target.value,
+										});
+									}}
+								></Textarea>
+							</InputDiv>
+							<ButtonDiv>
+								<SubmitButton
+									onClick={handleSubmit}
+									type="button"
+								>
+									Submit
+								</SubmitButton>
+							</ButtonDiv>
+						</form>
+					</FormDiv>
+					<PersonalLinkDiv>
+						<TileTitle>Contact me elsewhere</TileTitle>
+					</PersonalLinkDiv>
+				</RightDiv>
 			</SecondMainContainer>
 		</>
 	);
@@ -168,14 +176,56 @@ const Tagline = styled.p`
 
 const SecondMainContainer = styled(MainContainer)`
 	background-color: var(--primary-color);
-	height: 75vh; // was 100vh before
+	height: 100vh; // was 100vh before
+	max-width: 100%;
+
 `;
+
+const LeftDiv = styled.div`
+min-width: 40%;`;
 
 const ImageTitleWrapper = styled.div`
 	min-width: 40%;
 `;
 
+const MessageDiv = styled.div`
+	width: 350px;
+	height: 350px;
+	background-color: var(--secondary-color);
+	/* position: absolute; // I changed the structure of my section so the positioning is wrong. I'll change it later.
+	left: 0px;
+	top: 770px; */
+	border-radius: 5px;
+	padding: 20px 10px;
+`;
+
+const SubmitText = styled.p`
+	color: var(--tertiary-color);
+	font-family: var(--copy-font-family);
+	font-size: 20px;
+	text-align: justify;
+	padding: 10px 5px;
+	line-height: 115%;
+`;
+
+const RightDiv = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  grid-template-areas:
+  " tile1 tile1 tile1 tile1 ."
+  " tile1 tile1 tile1 tile1 ."
+  " tile1 tile1 tile1 tile1 ."
+  " . tile2 tile2 tile2 tile2"
+  " . tile2 tile2 tile2 tile2";
+  grid-column-gap: 10px;
+	grid-row-gap: 10px;
+  margin: 10px 0;
+  min-width: 60%;
+`;
+
 const FormDiv = styled.div`
+	grid-area: tile1;
 	position: relative;
 	z-index: 10;
 	background-color: var(--tertiary-color);
@@ -221,7 +271,6 @@ const InputField = styled.input`
 	border-radius: 2px;
 	border: 1px solid black;
 	box-sizing: border-box;
-	width: 400px;
 
 	&:focus {
 		border: 2px solid black;
@@ -290,24 +339,11 @@ const SubmitButton = styled.button`
 	}
 `;
 
-const MessageDiv = styled.div`
-	width: 350px;
-	height: 350px;
-	background-color: var(--secondary-color);
-	position: absolute;
-	left: 0px;
-	top: 770px;
+const PersonalLinkDiv = styled.div`
+	grid-area: tile2;
+  padding: 10px;
 	border-radius: 5px;
-  padding: 20px 10px;
-`;
-
-const SubmitText = styled.p`
-	color: var(--tertiary-color);
-	font-family: var(--copy-font-family);
-	font-size: 20px;
-  text-align: justify;
-  padding: 10px 5px;
-  line-height: 115%;
+  background-color: var(--secondary-color);
 `;
 
 export default Contact;
