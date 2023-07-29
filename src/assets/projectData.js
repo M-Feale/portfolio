2854b2b3af9b78c60751589f1ecbd1f74e75608d
M-Feale <marjolainefeale@gmail.com>
2023-07-29 17:23:59 -0400

    fixed-typo-in-props-value-and-modified-isColorSwapped-in-About-for-design-reasons

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -2,45 +2,44 @@ export const aboutMeTile = [
 	{
 		id: "tile1",
 		title: "Section 1",
-		isColorSwap: false,
+		isColorSwapped: false,
 		additionalText: [
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget nunc scelerisque viverra mauris in aliquam sem."
-		]
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget nunc scelerisque viverra mauris in aliquam sem.",
+		],
 	},
 	{
 		id: "tile2",
 		title: "Section 2",
-		isColorSwap: true,
+		isColorSwapped: true,
 		additionalText: [
 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget nunc scelerisque viverra mauris in aliquam sem."
-		]
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget nunc scelerisque viverra mauris in aliquam sem.",
+		],
 	},
 	{
 		id: "tile3",
 		title: "Section 3",
-		isColorSwap: true,
+		isColorSwapped: false,
 		additionalText: [
 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget nunc scelerisque viverra mauris in aliquam sem.",
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-		]
-	}
-]
-
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		],
+	},
+];
 
 export const tilesNyanCat = [
-	{   
-        id: "tile1",
+	{
+		id: "tile1",
 		title: "Purpose",
-		isColorSwap: false,
+		isColorSwapped: false,
 		additionalText: [
 			"Practice and Learn Object-Oriented-Programing with a Horizontal Dodging Game",
 		],
 	},
-	{   
-        id: "tile2",
+	{
+		id: "tile2",
 		title: "Objectives",
-		isColorSwap: true,
+		isColorSwapped: true,
 		additionalText: [
 			"Make the game end when player collides with enemy",
 			"Add game functionality and progression",
@@ -48,9 +47,9 @@ export const tilesNyanCat = [
 		],
 	},
 	{
-        id: "tile3",
+		id: "tile3",
 		title: "Process - Functionality",
-		isColorSwap: false,
+		isColorSwapped: false,
 		additionalText: [
 			"Researched the code base to acquire a satisfactory understanding",
 			"Refined collision logic for a robust X-axis and Y-axis hitbox",
@@ -58,10 +57,10 @@ export const tilesNyanCat = [
 			"Added vertical movement for protagonist",
 		],
 	},
-	{   
-        id: "tile4",
+	{
+		id: "tile4",
 		title: "Process - Game Experience",
-		isColorSwap: true,
+		isColorSwapped: true,
 		additionalText: [
 			"Enhanced gaming experience by adding player animation during collisions",
 			"Added start and reset buttons for fast and easy launch",
@@ -70,9 +69,9 @@ export const tilesNyanCat = [
 		],
 	},
 	{
-        id: "tile5",
+		id: "tile5",
 		title: "Process - Design",
-		isColorSwap: false,
+		isColorSwapped: false,
 		additionalText: [
 			"Developed several Javascript sprite animations for player character and enemies",
 			"Redesigned background and gameboard for a more pleasing visual experience",
